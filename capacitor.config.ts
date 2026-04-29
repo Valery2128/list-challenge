b669ad2171e91b52fecc75542c21d52d3ea2af24
Valery Morantes <jmorantes32@gmail.com>
2026-04-28 20:01:23 -0500

    chore: add capacitor config and final style adjustments

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.valery.listtask',
+  appName: 'ListTask',
+  webDir: 'www'
+};
+
+export default config;
